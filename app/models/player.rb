87eaa31984e461b08a27252743080a02dd5bbbd5
Marco Arias <mtarias@sudaoutdoors.com>
2019-07-16 01:13:10 -0400

    Add model method to calculate bet

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,14 @@
 class Player < ActiveRecord::Base
     validates :name, presence: true
+    def get_money_bet(is_rain=false)
+        if money <= 1000
+            return money        
+        elsif is_rain
+            percentage_bet = rand(4.0..10.0)/100.to_f
+            return (money*percentage_bet).to_i
+        else
+            percentage_bet = rand(8.0..15.0)/100.to_f
+            return (money*percentage_bet).to_i
+        end
+    end
 end
